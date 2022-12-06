5939cd4b77c61e606406b19fc2e422fa297000e0
Mohit Kumar <digitalboymksingh@gmail.com>
2022-12-07 00:57:16 +0530

    Add files via upload

---
+++ src/components/Feature/Feature.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { FeatureContainer, FeatureButton } from './FeatureElements';
+
+function Feature() {
+	return (
+		<FeatureContainer>
+			<h1>Offer of the Day</h1>
+			<p>GET free saving on Haircut*.</p>
+			<FeatureButton>Book Now</FeatureButton>
+		</FeatureContainer>
+	);
+}
+
+export default Feature;
